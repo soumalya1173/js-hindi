8ffa363bb3673ad51a47c44c467ee1a5f6edfa1e
Soumalya Roy <123768822+soumalya1173@users.noreply.github.com>
2024-02-24 15:15:56 +0000

    conversion

--- 01-basic/03_conversionOparetion.js
+++ 01-basic/03_conversionOparetion.js
@@ -1,1 +1,28 @@
-let score = 33
+let score = "33";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+//**********Operations*************/
+
+let value = 3
+let nrgValue =-value
+console.log(nrgValue);
+
+let str1 ="Hello"
+let str2 =" Soumalya"
+let str3 =str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log( 3);
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
