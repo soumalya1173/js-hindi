ad2f8053833d3237dc668de7cdfd60d81828748e
Soumalya Roy <123768822+soumalya1173@users.noreply.github.com>
2024-02-24 15:15:41 +0000

    alll

---
+++ 01-basic/01_veriables.js
@@ -0,0 +1,19 @@
+const accountId= 144553;
+let accountEmail="soumalya"
+var accountPasssword="12345"
+accountCity="Jajpur"
+let accountState;
+
+//accountId=2;
+
+accountEmail="45543t5"
+accountPasssword="ssfgfeg"
+accountCity="Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issuue in block scope and funtional scope
+*/
+console.table([accountId, accountEmail, accountPasssword, accountCity])
---
+++ 01-basic/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) //we are using node.js, not browser
+
+console.log(3 + 3);
+console.log("soumalya");
+
+let name="hitesh";
+let age=18;
+let isLoggedIn= false
+// number => 2 to power 53
+//bigint
+//string =>""
+// boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof "soumalya");
+
+// null type object
